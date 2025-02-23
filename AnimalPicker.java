3c01e1ecabb4740b3c32451fdea2a6987a74a207
aditibaghel9 <asbaghel@quinnipiac.edu>
2025-02-23 17:11:47 -0500

    Added a while statement for if the user doesn't type the correct string

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -6,27 +6,32 @@ public void animalOption(){
 
     System.out.println("Would you like to see a dog or cat? Enter D for dog or C for cat.");
 
+    String dog = "D";
+    String cat = "C";
+
     Scanner sc = new Scanner(System.in);
 
-    String option = sc.next();
+    String option = sc.nextLine(); 
         
-        if(option.equals("D")){
-
-            System.out.println("DOG ASCII ART HERE");
-        }
-
-        else if(option.equals("C")){
+    while(!option.equals(dog) && !option.equals(cat))
     
-            System.out.println("CAT ASCII ART HERE");
-            }
-    
+    {
+        System.out.println("Invalid Statement");
 
-        else {
-
-            System.out.println("Invalid Statement");
-        }
+        System.out.println("Please enter D for dog or C for cat.");
+        option = sc.nextLine();
     }
 
+    if(option.equals(dog)) {
+           
+        System.out.println("DOG ASCII ART HERE");   
+   }
+
+   else if(option.equals(cat)){
+
+        System.out.println("CAT ASCII ART HERE");   
+   }       
+}       
     
     public static void main(String[] args){
 
